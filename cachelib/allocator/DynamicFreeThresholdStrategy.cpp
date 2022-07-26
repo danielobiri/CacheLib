b428217e9a50a39b597b39d4e224ff5f9feaf2f0
Boglarka Ecsedi <boglarka.ecsedi@intel.com>
2022-07-26 13:29:25 -0400

    Updated contructor

--- cachelib/allocator/DynamicFreeThresholdStrategy.cpp
+++ cachelib/allocator/DynamicFreeThresholdStrategy.cpp
@@ -16,21 +16,11 @@
 
  //class-specific dynamic free high threshold strategy
 
- //NOTES!!!!
- //NEED TO:
- // 1) fix the accesses to certain functions to get the dimensions (num of tiers, pools, classes)
- // 2) change the benefit function to work with class-level allocation latency
- //allocLatencyNs.estimate() from AllocationStats
- //THEN
- // we can test it!
 
 #include "cachelib/allocator/DynamicFreeThresholdStrategy.h"
-#include "cachelib/allocator/Cache.h"
-#include "cachelib/allocator/CacheStats.h"
 #include "cachelib/allocator/memory/MemoryPoolManager.h"
 #include "cachelib/allocator/memory/MemoryAllocator.h"
-#include <vector>
-#include <tuple>
+#include "cachelib/allocator/Cache.h"
 #include <folly/logging/xlog.h>
 
 namespace facebook {
@@ -39,32 +29,60 @@ namespace cachelib {
 
 
 DynamicFreeThresholdStrategy::DynamicFreeThresholdStrategy(double lowEvictionAcWatermark, double highEvictionAcWatermark, uint64_t maxEvictionBatch, uint64_t minEvictionBatch)
-    : lowEvictionAcWatermark(lowEvictionAcWatermark), highEvictionAcWatermark(highEvictionAcWatermark), maxEvictionBatch(maxEvictionBatch), minEvictionBatch(minEvictionBatch) {
-        auto numTiers = CacheBase::kMaxTiers;
-        auto numPools = MemoryPoolManager::kMaxPools;
-        auto numClasses = MemoryAllocator::kMaxClasses;
-        for (int i = 0; i < numTiers; i++) {
-          std::vector<std::vector<std::tuple<double, double, double>>> poolHighThresholds;
-          std::vector<std::vector<std::tuple<double, double>>> poolBenefits;
-          std::vector<std::vector<std::tuple<double, double>>> poolToFreeMemPercents;
-          for (int j = 0; j < numPools; j++) {
-            std::vector<std::tuple<double, double, double>> classHighThresholds;
-            std::vector<std::tuple<double, double>> classBenefits;
-            std::vector<std::tuple<double, double>> classToFreeMemPercents;
-            for (int k = 0; k < numClasses; k++) {
-                classHighThresholds.push_back(std::make_tuple(highEvictionAcWatermark, highEvictionAcWatermark, highEvictionAcWatermark));
-                classBenefits.push_back(std::make_tuple(0.0, 0.0));
-                classToFreeMemPercents.push_back(std::make_tuple(0.0, 0.0));
+    : lowEvictionAcWatermark(lowEvictionAcWatermark), highEvictionAcWatermark(highEvictionAcWatermark), maxEvictionBatch(maxEvictionBatch), minEvictionBatch(minEvictionBatch), 
+    highEvictionAcWatermarks(CacheBase::kMaxTiers, 
+                                std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools,
+                                std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses,
+                                std::vector<double>(3)))),
+    acBenefits(CacheBase::kMaxTiers, 
+                  std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools,
+                  std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses,
+                  std::vector<double>(2)))),
+    acToFreeMemPercents(CacheBase::kMaxTiers,
+                        std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools,
+                        std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses,
+                        std::vector<double>(2)))) {
+      /*
+      auto numTiers = CacheBase::kMaxTiers;
+      auto numPools = MemoryPoolManager::kMaxPools;
+      auto numClasses = MemoryAllocator::kMaxClasses;
+      for (int i = 0; i < numTiers; i++) {
+        for (int j = 0; j < numPools; j++) {
+          for (int k = 0; k < numClasses; k++) {
+            for (int l = 0; l < 3; l++) {
+              highEvictionAcWatermarks[i][j][k][l] = 5.0;
             }
-            poolHighThresholds.push_back(classHighThresholds);
-            poolBenefits.push_back(classBenefits);
-            poolToFreeMemPercents.push_back(classToFreeMemPercents);
+            for (int m = 0; m < 2; m++) {
+              acBenefits[i][j][k][m] = 0.0;
+              acToFreeMemPercents[i][j][k][m] = 0.0;
+            }
           }
-          highEvictionAcWatermarks.push_back(poolHighThresholds);
-          acBenefits.push_back(poolBenefits);
-          acToFreeMemPercents.push_back(poolToFreeMemPercents);
         }
       }
+
+      for (int i = 0; i < numTiers; i++) {
+        std::vector<std::vector<std::vector<double>>> poolHighThresholds;
+        std::vector<std::vector<std::vector<double>>> poolBenefits;
+        std::vector<std::vector<std::vector<double>>> poolToFreeMemPercents;
+        for (int j = 0; j < numPools; j++) {
+          std::vector<std::vector<double>> classHighThresholds;
+          std::vector<std::vector<double>> classBenefits;
+          std::vector<std::vector<double>> classToFreeMemPercents;
+          for (int k = 0; k < numClasses; k++) {
+            classHighThresholds.push_back(std::vector<double>{5.0, 5.0, 5.0});
+            classBenefits.push_back(std::vector<double>{0.0, 0.0});
+            classToFreeMemPercents.push_back(std::vector<double>{0.0, 0.0});
+          }
+          poolHighThresholds.push_back(classHighThresholds);
+          poolBenefits.push_back(classBenefits);
+          poolToFreeMemPercents.push_back(classToFreeMemPercents);
+        }
+        highEvictionAcWatermarks.push_back(poolHighThresholds);
+        acBenefits.push_back(poolBenefits);
+        acToFreeMemPercents.push_back(poolToFreeMemPercents);
+      }
+      */
+    }
 
 std::vector<size_t> DynamicFreeThresholdStrategy::calculateBatchSizes(const CacheBase& cache, std::vector<std::tuple<TierId, PoolId, ClassId>> acVec) {
   
@@ -73,15 +91,15 @@ std::vector<size_t> DynamicFreeThresholdStrategy::calculateBatchSizes(const Cach
   for (auto [tid, pid, cid] : acVec) {
     auto stats = cache.getAllocationClassStats(tid, pid, cid);
     auto acFree = stats.approxFreePercent;
-    auto acHighThresholdAtI = std::get<0>(highEvictionAcWatermarks[tid][pid][cid]);
+    auto acHighThresholdAtI = highEvictionAcWatermarks[tid][pid][cid][0];
 
     if (acFree >= acHighThresholdAtI) {
       batches.push_back(0);
     } else {
-      auto acHighThresholdAtIMinus1 = std::get<1>(highEvictionAcWatermarks[tid][pid][cid]);
-      auto acHighThresholdAtIMinus2 = std::get<2>(highEvictionAcWatermarks[tid][pid][cid]);
-      auto acHighThresholdAtINew = std::get<0>(highEvictionAcWatermarks[tid][pid][cid]);
-      auto toFreeMemPercentAtIMinus1 = std::get<1>(acToFreeMemPercents[tid][pid][cid]);
+      auto acHighThresholdAtIMinus1 = highEvictionAcWatermarks[tid][pid][cid][1];
+      auto acHighThresholdAtIMinus2 = highEvictionAcWatermarks[tid][pid][cid][2];
+      auto acHighThresholdAtINew = highEvictionAcWatermarks[tid][pid][cid][0];
+      auto toFreeMemPercentAtIMinus1 = acToFreeMemPercents[tid][pid][cid][1];
       auto acAllocLatencyNs = cache.getAllocationClassStats(tid, pid, cid).allocLatencyNs.estimate();
       
       calculateBenefitMig(acAllocLatencyNs, tid, pid, cid);
@@ -89,7 +107,7 @@ std::vector<size_t> DynamicFreeThresholdStrategy::calculateBatchSizes(const Cach
       if (toFreeMemPercentAtIMinus1 < acFree / 2) {
         acHighThresholdAtINew =- highEvictionDelta;
       } else {
-        if (std::get<0>(acBenefits[tid][pid][cid]) > std::get<1>(acBenefits[tid][pid][cid])) {
+        if (acBenefits[tid][pid][cid][0] > acBenefits[tid][pid][cid][1]) {
           if (acHighThresholdAtIMinus1 > acHighThresholdAtIMinus2) {
             acHighThresholdAtINew += highEvictionDelta;
           } else {
@@ -105,19 +123,15 @@ std::vector<size_t> DynamicFreeThresholdStrategy::calculateBatchSizes(const Cach
       }
       
       auto toFreeMemPercent = acHighThresholdAtINew - acFree;
-      std::get<1>(acToFreeMemPercents[tid][pid][cid]) = toFreeMemPercent;
+      acToFreeMemPercents[tid][pid][cid][1] = toFreeMemPercent;
       auto toFreeItems = static_cast<size_t>(toFreeMemPercent * stats.memorySize / stats.allocSize);
       batches.push_back(toFreeItems);
 
-      std::get<0>(highEvictionAcWatermarks[tid][pid][cid]) = acHighThresholdAtINew;
-      std::get<1>(highEvictionAcWatermarks[tid][pid][cid]) = acHighThresholdAtI;
-      std::get<2>(highEvictionAcWatermarks[tid][pid][cid]) = acHighThresholdAtIMinus1;
+      highEvictionAcWatermarks[tid][pid][cid][0] = acHighThresholdAtINew;
+      highEvictionAcWatermarks[tid][pid][cid][1] = acHighThresholdAtI;
+      highEvictionAcWatermarks[tid][pid][cid][2] = acHighThresholdAtIMinus1;
     }
   }
-
-  //TODO: add class-based allocation latency for highEvictionWatermark
-  //TODO: add class-based read latency for lowEvictionWatermark
-  //auto latencies = cache.getAllocationLatency();
 
   if (batches.size() == 0) {
     return batches;
@@ -143,9 +157,9 @@ std::vector<size_t> DynamicFreeThresholdStrategy::calculateBatchSizes(const Cach
 }
 
 void DynamicFreeThresholdStrategy::calculateBenefitMig(uint64_t acLatency, unsigned int tid, PoolId pid, ClassId cid) {
-    auto currentBenefit = std::get<0>(acBenefits[tid][pid][cid]);
-    std::get<1>(acBenefits[tid][pid][cid]) = currentBenefit;
-    std::get<0>(acBenefits[tid][pid][cid]) = 1 / acLatency;
+    auto currentBenefit = acBenefits[tid][pid][cid][0];
+    acBenefits[tid][pid][cid][1] = currentBenefit;
+    acBenefits[tid][pid][cid][0] = 1 / acLatency;
 }
 
 BackgroundStrategyStats DynamicFreeThresholdStrategy::getStats() { 
--- cachelib/allocator/DynamicFreeThresholdStrategy.h
+++ cachelib/allocator/DynamicFreeThresholdStrategy.h
@@ -18,13 +18,10 @@
 
 #pragma once
 
-#include "cachelib/allocator/Cache.h"
 #include "cachelib/allocator/BackgroundEvictorStrategy.h"
+#include "cachelib/allocator/Cache.h"
 #include "cachelib/allocator/CacheStats.h"
-#include "cachelib/allocator/memory/MemoryPoolManager.h"
-#include "cachelib/allocator/memory/MemoryAllocator.h"
 #include <vector>
-#include <tuple>
 
 namespace facebook {
 namespace cachelib {
@@ -35,6 +32,8 @@ class DynamicFreeThresholdStrategy : public BackgroundEvictorStrategy {
 
 public:
   DynamicFreeThresholdStrategy(double lowEvictionAcWatermark, double highEvictionAcWatermark, uint64_t maxEvictionBatch, uint64_t minEvictionBatch);
+  //std::vector<std::vector<std::vector<std::vector<double>>>> highEvictionAcWatermarks, std::vector<std::vector<std::vector<std::vector<double>>>> acBenefits, 
+  //std::vector<std::vector<std::vector<std::vector<double>>>> acToFreeMemPercents);
   ~DynamicFreeThresholdStrategy() {}
 
   std::vector<size_t> calculateBatchSizes(const CacheBase& cache, std::vector<std::tuple<TierId, PoolId, ClassId>> acVec);
@@ -43,24 +42,38 @@ public:
 
 private:
   double lowEvictionAcWatermark{2.0}; //this threshold is used outside this class and is not adjusted currently
-  double highEvictionAcWatermark{5.0}; //this threshold is adjusted internally within this class
+  double highEvictionAcWatermark{5.0}; //this threshold is adjusted internally, individually for each ac class
   uint64_t maxEvictionBatch{0};
   uint64_t minEvictionBatch{0};
-  //double toFreeMemPercent{0.0}; //Q: What happens to this value when the background thread is not activated in a certain period in Class x? Should we set it to 0?
-  std::vector<std::vector<std::vector<std::tuple<double, double>>>> acToFreeMemPercents; //logging toFreeMemPercents for comparison
-  double highEvictionDelta{1.0}; //TODO: tune this param, experiment with multiple values, (maybe base it on access freq or other access stat), perhaps use the benefit function to adjust this param (binned)?
-  std::vector<std::vector<std::vector<std::tuple<double, double, double>>>> highEvictionAcWatermarks;
-  //individual thresholds for each class, adjusted internally within this class
+  double highEvictionDelta{0.5}; //TODO: tune this param, experiment with multiple values, (maybe base it on access freq or other access stat), perhaps use the benefit function to adjust this param (binned)?
+
+  std::vector<std::vector<std::vector<std::vector<double>>>> highEvictionAcWatermarks; //individual dynamic thresholds for each ac class
   //index 0 for i-th window
   //index 1 for i-1 window
   //index 2 for i-2 window
 
-  //std::array<std::array<std::array<MemoryAllocator::kMaxClasses>, MemoryPoolManager::kMaxPools>, CacheBase::kMaxTiers> highEvictionAcWatermarks; //use array instead w these dimensions
-
-  std::vector<std::vector<std::vector<std::tuple<double, double>>>> acBenefits;
+  std::vector<std::vector<std::vector<std::vector<double>>>> acBenefits;
   //index 0 for current benefit (i-th window)
   //index 1 for previous benefit (i-1 window)
+  
+  std::vector<std::vector<std::vector<std::vector<double>>>> acToFreeMemPercents; //logging toFreeMemPercents for comparison
+  //index 0 for current toFreeMemPercent (i-th window)
+  //index 1 for previous toFreeMemPercent (i-1 window)
 
+  //CacheBase::kMaxTiers, 
+  //std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools, 
+  //std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses, 
+  //std::vector<double>(3, 5.0)));
+
+  //CacheBase::kMaxTiers, 
+  //std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools, 
+  //std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses, 
+  //std::vector<double>(2, 0.0)));
+
+  //CacheBase::kMaxTiers, 
+  //std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools, 
+  //std::vector<std::vector<double>>(MemoryAllocator::kMaxClasses, 
+  //std::vector<double>(2, 0.0)));
 
 private:
   void calculateBenefitMig(uint64_t p99, unsigned int tid, PoolId pid, ClassId cid);
