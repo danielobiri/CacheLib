b1f2f3e5acac26d832e638496881549f11c8b32d
Daniel Ananey-Obiri <62302457+danielobiri@users.noreply.github.com>
2022-11-07 13:10:01 -0500

    Update DynamicFreeThresholdStrategy.cpp

--- cachelib/allocator/DynamicFreeThresholdStrategy.cpp
+++ cachelib/allocator/DynamicFreeThresholdStrategy.cpp
@@ -34,7 +34,7 @@ namespace cachelib {
 
 
 
-DynamicFreeThresholdStrategy::DynamicFreeThresholdStrategy(double lowEvictionAcWatermark, double highEvictionAcWatermark, uint64_t maxEvictionBatch, uint64_t minEvictionBatch, double highEvictionDelt )
+DynamicFreeThresholdStrategy::DynamicFreeThresholdStrategy(double lowEvictionAcWatermark, double highEvictionAcWatermark, uint64_t maxEvictionBatch, uint64_t minEvictionBatch, double highEvictionDelta )
     : lowEvictionAcWatermark(lowEvictionAcWatermark), highEvictionAcWatermark(highEvictionAcWatermark), maxEvictionBatch(maxEvictionBatch), minEvictionBatch(minEvictionBatch), highEvictionDelta(highEvictionDelta), 
     highEvictionAcWatermarks(CacheBase::kMaxTiers, 
                                 std::vector<std::vector<std::vector<double>>>(MemoryPoolManager::kMaxPools,
